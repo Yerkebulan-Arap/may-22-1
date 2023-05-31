c8c36a331c838c8c4d84398c67d4525ece6c7bc7
Yerkebulan <134504390+Yerkebulan-Arap@users.noreply.github.com>
2023-05-31 10:12:51 +0600

    Add files via upload

---
+++ work1.py
@@ -0,0 +1,7 @@
+user_input = str(input("Введите слово из трех букв:"))
+bukva1 = ord(user_input[0])
+bukva2 = ord(user_input[1])
+bukva3 = ord(user_input[2])
+result = bukva1+bukva2+bukva3
+
+print("Результат:", result)
